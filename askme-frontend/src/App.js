619f66ecef3db25fb59b3d7672fa5ba9ee0ea4c8
titosilva <jt.mat@hotmail.com>
2021-04-08 10:40:37 -0300

    feat: added and configured eslint

--- askme-frontend/src/App.js
+++ askme-frontend/src/App.js
@@ -1,6 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import {React} from 'react';
 
+/**
+ * Entry point
+ * @return {Component} App root
+ */
 function App() {
   return (
     <div className="App">
