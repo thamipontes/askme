fb8810e702425c9ebc18ada787d3a41882d0cd1b
titosilva <jt.mat@hotmail.com>
2021-04-24 22:15:26 -0300

    [I-23] feat: added Quiz entity

---
+++ askme-backend/src/entities/quiz.test.js
@@ -0,0 +1,49 @@
+const Quiz = require('./quiz');
+
+test('getSchema should return correct schema', () => {
+  expect(Quiz.getSchema()).toStrictEqual({
+    creatorId: String,
+    title: String,
+    isAnonymous: Boolean,
+    xml: String,
+  });
+});
+
+test('toObject should convert properties properly', () => {
+  const quiz = new Quiz('userId', 'abcdef', true);
+  expect(quiz.toObject()).toStrictEqual({
+    creatorId: 'userId',
+    title: 'abcdef',
+    isAnonymous: true,
+    xml: '', // TODO: improve this test case with xml conversion
+  });
+});
+
+test('isValid should return true when properties are valid', () => {
+  const quiz = new Quiz('userId', 'abcdef', true);
+  expect(quiz.isValid()).toBe(true);
+});
+
+test(
+    'isValid should return false when any properties are null or undefined',
+    () => {
+      let quiz = new Quiz(null, 'abcdef', true);
+      expect(quiz.isValid()).toBe(false);
+
+      quiz = new Quiz('userId', null, true);
+      expect(quiz.isValid()).toBe(false);
+
+      quiz = new Quiz('userId', 'abcdef', null);
+      expect(quiz.isValid()).toBe(false);
+    },
+);
+
+test('isValid should return false when quiz has too little title', () => {
+  const quiz = new Quiz('userId', 'abcd', true);
+  expect(quiz.isValid()).toBe(false);
+});
+
+test('isValid should return false when quiz has too big title', () => {
+  const quiz = new Quiz('userId', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', true);
+  expect(quiz.isValid()).toBe(false);
+});
