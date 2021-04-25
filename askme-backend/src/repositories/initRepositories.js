6612aea7ef089d111bdb779cf0b8da61bde8fcae
titosilva <jt.mat@hotmail.com>
2021-04-25 09:10:09 -0300

    [I-14] feat: added QuizRepository to initRepositories

--- askme-backend/src/repositories/initRepositories.js
+++ askme-backend/src/repositories/initRepositories.js
@@ -1,6 +1,7 @@
 const UserRepository = require('./userRepository');
 // eslint-disable-next-line no-unused-vars
 const mongoose = require('mongoose');
+const QuizRepository = require('./quizRepository');
 
 /**
  * Inits all repositories, passing the given mongoose instance
@@ -9,7 +10,10 @@ const mongoose = require('mongoose');
 function initAllRepositories(mongoose) {
   console.log('Initing UserRepository...');
   UserRepository.init(mongoose);
-  console.log('UserRepository inited successfully!');
+  console.log('Initing QuizRepository...');
+  QuizRepository.init(mongoose);
+
+  console.log('Repositories inited successfully');
 }
 
 module.exports = initAllRepositories;
